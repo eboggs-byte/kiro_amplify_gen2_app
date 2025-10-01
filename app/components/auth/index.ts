feb61efb191832c5c777c144b591d455d911f254
eric <eric.boggs@allcloud.io>
2025-10-01 13:03:02 -0400

    Remove all unused components currently

--- app/components/auth/index.ts
+++
@@ -1,7 +0,0 @@
-// Auth Components Barrel Export
-export { default as AuthenticatorWrapper } from './AuthenticatorWrapper';
-export { default as AuthHeader } from './AuthHeader';
-export { default as AuthFooter } from './AuthFooter';
-export { SignInHeader, SignInFooter } from './SignInComponents';
-export { SignUpHeader, SignUpFooter } from './SignUpComponents';
-export { authTheme } from './authTheme';
