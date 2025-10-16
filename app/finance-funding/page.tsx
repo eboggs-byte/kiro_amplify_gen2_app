5b68f757606c92b89e4afd9286c258eb20498548
eric <eric.boggs@allcloud.io>
2025-10-16 15:37:55 -0400

    begin refractor for prod

--- app/finance-funding/page.tsx
+++ app/finance-funding/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import FinanceFundingWorkflow from '../components/workflows/FinanceFundingWorkflow';
-import ListDynamoTables from '../components/ListDynamoTables';
+// import ListDynamoTables from '../components/ListDynamoTables';
 
 export default function FinanceFundingPage() {
     return (
         <div className="space-y-6">
-            {/* Real DynamoDB Tables List */}
-            <ListDynamoTables />
+            {/* Real DynamoDB Tables List - Commented out for production */}
+            {/* <ListDynamoTables /> */}
             
             <FinanceFundingWorkflow />
         </div>
